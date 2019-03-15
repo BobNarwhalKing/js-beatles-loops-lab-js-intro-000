d1126b61c70d1cc9384022d427b3015f12250daa
Kurtis Leach <danceflurry4366@gmail.com>
2019-03-15 19:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ function theBeatlesPlay (musicians, instruments) {
 }
 return array
 }
-const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
 
 function johnLennonFacts(facts) {
-  var array = [];
+  var array = []
   let i = 0;
   while (facts.length > i) {
     array.push(`${facts[i]}!!!`)
     i++
   }
-  return array;
+  return array
+}
+function iLoveTheBeatles (n) {
+  var array = []
+  do {('I love the Beatles!')
+  n++
+}
+while (n < 15)
+return array
 }
